3e12033baa1c40cf3361fc49eb988ace87ca529d
sriram <sriram@localhost.localdomain>
2019-06-29 18:07:37 -0700

    adding vtl device common file

---
+++ h/vtl-device-names.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright - Nubos Networks
+ *
+ * Author - Sri
+ *
+ */
+
+#ifndef _VTL_DEVICE_NAMES_H_
+#define _VTL_DEVICE_NAMES_H_
+
+/* this is the file included user 
+ * space and kernel 
+ */
+#define VSL_DEVICE_NAME "vsl"
+#define RS_DEVICE_NAME "eth1"
+
+#endif
